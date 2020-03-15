b62b70f305952f41137aaf00baf9a37598b71b76
Edward Schaefer <EdwardSchaefer@users.noreply.github.com>
2020-03-14 22:13:21 -0400

    Add offset for handles. Use Vector for coordinates.

--- src/app/envelope/envelope.component.ts
+++ src/app/envelope/envelope.component.ts
@@ -14,21 +14,23 @@ export class EnvelopeComponent {
   height = 200;
   width = this.height * this.nodeCount / 2;
   constructor(public synth: SynthService) {
-    this.envelope = new Envelope(this.nodeCount);
+    this.envelope = new Envelope(this.nodeCount, this.height);
   }
   update(event, handle: (VerticalHandle | BezierHandle)) {
     const transform: string = event.source.element.nativeElement.style.transform;
     const coords = transform.substring(12).split(',');
-    handle.cx = parseInt(coords[0].split('px')[0], 10) + handle.initX;
-    handle.cy = parseInt(coords[1].split('px')[0], 10) + handle.initY;
-    const handles = this.envelope.handles.map(envHandle => new Vector(envHandle.cx, envHandle.cy));
+    handle.coord.x = parseInt(coords[0].split('px')[0], 10) + handle.initX;
+    handle.coord.y = parseInt(coords[1].split('px')[0], 10) + handle.initY;
+    const handles = this.envelope.handles.map(envHandle => new Vector(envHandle.coord.x, envHandle.coord.y));
     this.synth.curve.next(handles);
   }
 }
 
 class Envelope {
   handles: (VerticalHandle[] | BezierHandle[]) = [];
-  constructor(nodes: number) {
+  height: number;
+  constructor(nodes: number, height: number) {
+    this.height = height;
     for (let i = 0; i < nodes; i++) {
       this.handles.push(i % 2 ? new BezierHandle(i) : new VerticalHandle(i));
     }
@@ -36,12 +38,11 @@ class Envelope {
   get path(): string {
     let result = '';
     this.handles.forEach((handle, index) => {
-      const coordString = handle.cx + ' ' + handle.cy + ' ';
       let svgCommand = 'M';
       if (index) {
         svgCommand = index % 2 ? 'Q' : '';
       }
-      result = result + svgCommand + coordString;
+      result = result + svgCommand + handle.coordString;
     });
     return result;
   }
--- src/app/handle-model.ts
+++ src/app/handle-model.ts
@@ -1,21 +1,24 @@
+import {Vector} from './vector-model';
+
 class Handle {
   radius: number;
   fill: string;
   index: number;
   initX: number;
   initY: number;
-  cx: number;
-  cy: number;
+  coord: Vector;
   lockAxis: string;
   dragBoundary: string;
   rectX: number;
   rectWidth: number;
   constructor(i: number) {
     this.index = i;
-    this.initX = 100 * i;
+    this.initX = 10 + (100 * i);
     this.initY = 100;
-    this.cx = 100 * i;
-    this.cy = 100;
+    this.coord = new Vector(100 * i, 100);
+  }
+  get coordString() {
+    return this.coord.x + ' ' + this.coord.y + ' ';
   }
 }
 
@@ -38,7 +41,7 @@ export class BezierHandle extends Handle {
     this.fill = 'grey';
     this.lockAxis = '';
     this.dragBoundary = '.handle-container';
-    this.rectX = (i * 100) - 100;
+    this.rectX = 10 + ((i * 100) - 100);
     this.rectWidth = 200;
   }
 }
